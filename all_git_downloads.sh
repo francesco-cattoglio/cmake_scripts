8eb487c22209d8a131ba2b40c7ac2cb6aab9f3de
francesco cattoglio <francesco.cattoglio@gmail.com>
2019-02-28 11:52:16 +0100

    More changes to make it more difficult to make mistakes
    1: now we use the env var CMAKE_BUILDS_PREFIX, this should make it
    clear that it is not only the value for CMAKE_INSTALL_PREFIX anymore
    2: do not use make, instead use cmake subcommand for building everything

--- all_git_downloads.sh
+++ all_git_downloads.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/francesco-cattoglio/imgui_for_dcs.git
-mkdir ./imgui_for_dcs/build
-git clone https://github.com/francesco-cattoglio/muparser_for_dcs.git
-mkdir ./muparser_for_dcs/build
-git clone https://github.com/mosra/magnum-integration.git
-mkdir ./magnum-integration/build
-git clone https://github.com/mosra/magnum.git
-mkdir ./magnum/build
-git clone https://github.com/mosra/magnum-plugins.git
-mkdir ./magnum-plugins/build
-git clone https://github.com/mosra/corrade.git
-mkdir ./corrade/build
-git clone https://github.com/glfw/glfw.git
-mkdir ./glfw/build
+git clone https://github.com/glfw/glfw.git && mkdir ./glfw/build
+git clone https://github.com/francesco-cattoglio/imgui_for_dcs.git && mkdir ./imgui_for_dcs/build
+git clone https://github.com/francesco-cattoglio/muparser_for_dcs.git && mkdir ./muparser_for_dcs/build
+git clone https://github.com/mosra/corrade.git && mkdir ./corrade/build
+git clone https://github.com/mosra/magnum.git && mkdir ./magnum/build
+git clone https://github.com/mosra/magnum-integration.git && mkdir ./magnum-integration/build
+git clone https://github.com/mosra/magnum-plugins.git && mkdir ./magnum-plugins/build
 
 echo "Now cloning DCS, which might require your github account to access"
-git clone https://github.com/francesco-cattoglio/dcs.git
+git clone https://github.com/francesco-cattoglio/dcs.git && mkdir ./dcs/build
 
+echo "cloning done. Remember to ``export CMAKE_BUILDS_PREFIX=/path/for/cmake/installations/'' before running the scripts"
+echo "suggested lib build order is: glfw, muparser, imgui, corrade, magnum, magnum-integration, magnum-plugins and finally dcs"
--- imgui.sh
+++ imgui.sh
@@ -1,2 +1,1 @@
-export IMGUI_FOR_DCS_PATH=$(pwd)/.. && echo "imgui path exported succesfully"
-
+export IMGUI_FOR_DCS_PATH=$(pwd)/.. && echo "imgui path exported succesfully, nothing else to do here"
