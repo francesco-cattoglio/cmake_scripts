68128ef31f2177343c3e6e42e79c0baae2b5c905
francesco-cattoglio <francesco.cattoglio@gmail.com>
2019-05-27 06:58:13 -0700

    Added a mingw64-only cmake definition to build with ANGLE

--- dcs.sh
+++ dcs.sh
@@ -3,4 +3,9 @@ cmake -DCMAKE_PREFIX_PATH=$CMAKE_BUILDS_PREFIX -DCMAKE_INSTALL_PREFIX=$CMAKE_BUI
        -DIMGUI_DIR=$IMGUI_FOR_DCS_PATH \
        -DCMAKE_BUILD_TYPE=${DCS_BUILD_TYPE:-Debug} \
        -G"Ninja" ..
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo "Configuring with support for SDL2/ANGLE"
+    cmake -DDCS_WITH_SDL2=TRUE -DDCS_WITH_GLES3=TRUE .
+fi
 cmake --build .
--- magnum.sh
+++ magnum.sh
@@ -4,6 +4,11 @@ cmake -DCMAKE_PREFIX_PATH=$CMAKE_BUILDS_PREFIX -DCMAKE_INSTALL_PREFIX=$CMAKE_BUI
       -DWITH_ANYIMAGEIMPORTER=true \
       -DCMAKE_BUILD_TYPE=${DCS_BUILD_TYPE:-Debug} \
       -G"Ninja" ..
-cmake -DWITH_SDL2APPLICATION=ON . || cmake -DWITH_SDL2APPLICATION=OFF ..
-cmake -DWITH_GLFWAPPLICATION=ON . || cmake -DWITH_GLFWAPPLICATION=OFF ..
+cmake -DWITH_SDL2APPLICATION=ON . || cmake -DWITH_SDL2APPLICATION=OFF .
+cmake -DWITH_GLFWAPPLICATION=ON . || cmake -DWITH_GLFWAPPLICATION=OFF .
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+    echo "Configuring with support for SDL2/ANGLE"
+    cmake -DTARGET_GLES=TRUE -DTARGET_GLES2=FALSE .
+fi
 cmake --build . --target install
